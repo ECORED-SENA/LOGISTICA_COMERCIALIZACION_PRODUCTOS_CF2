d6a1da10ae08875cc37b66ba02d10f96e95ffb62
Luis Pérez <ljpm9401@gmail.com>
2021-12-15 11:17:43 -0500

    Ajuste video

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -11,7 +11,7 @@
     p(data-aos="fade-up") Para el desarrollo de la economía de una zona o territorio, es imprescindible potenciar las actividades de logística y transporte, ya que son componentes claves de la competitividad. Asimismo, para aumentar su competitividad, el sector industrial y productivo, se ven obligados fundamentalmente a minimizar precios de la logística y el transporte que gravan el costo de sus productos. En el siguiente recurso de aprendizaje podrá conocer sobre el transporte y la gestión de logística.
     figure.mt-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/S1aBcNOe5tA" title="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/e4znUHgX9LY" title="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     
 
 </template>
